d4311e5e9856faabc04efeb742bb57929a3008c8
Rushali Lavangade <141850127+RushaliLavangade@users.noreply.github.com>
2023-11-30 12:35:16 +0000

    conversion of datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+// let somenumber = 33
+// let stringnumber = String(somenumber)
+// console.log(stringnumber)
+
+
+let isLoggedIn = "Rushali"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
